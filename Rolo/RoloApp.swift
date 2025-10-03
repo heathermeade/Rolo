3200e87e6c40ef24af008bdc36e31bb40695cf7b
Heather Meade <hmeade@usc.edu>
2025-10-02 19:09:26 -0700

    Initial Commit

---
+++ Rolo/RoloApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoloApp.swift
+//  Rolo
+//
+//  Created by Heather Meade on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct RoloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
